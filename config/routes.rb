ecfa00a3d1f9b5fa06421ef2d816a70fdff821e8
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-09-30 15:31:19 +1000

    Add action cables

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  # get 'chat_rooms/index'
 
-  # get 'chat_rooms/new'
-  resources :chat_rooms, only: [:index, :new]
-
+  resources :chat_rooms, only: [:index, :new, :create, :show]
   devise_for :users
+  
+  mount ActionCable.server => '/cable'
+
+  root 'chat_rooms#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
